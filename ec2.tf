e496f5610cd41f80a2cace2e8a7a596de0ba17b0
Raghunath Kushawaha <78621381+iamraghu149@users.noreply.github.com>
2024-04-16 16:54:23 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = ami-09298640a92b2d12c
-  instance_type = "t3.micro"
+  ami = "ami-09298640a92b2d12c"
+  instance_type = "t2.micro"
+  count = 5
 
   tags = {
     Name = "HelloWorld"
