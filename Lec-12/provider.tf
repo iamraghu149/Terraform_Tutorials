7aac2c4b8853d6161de0d2d4552b34c6a9939d39
Raghunath Kushawaha <78621381+iamraghu149@users.noreply.github.com>
2024-04-23 13:47:06 +0530

    Updated by Raghu ReadME
    Updated by Raghu ReadME

--- Lec-12/provider.tf
+++ Lec-12/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-docss"
+    dynamodb_table = "state-lock"
+    key = "global/mystatefile/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
