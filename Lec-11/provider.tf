46c0c0eab8061343a3eee718d8e34ff92ab2378e
Raghunath Kushawaha <78621381+iamraghu149@users.noreply.github.com>
2024-04-23 11:22:46 +0530

    Updated By Raghu
    Updated By Raghu

--- Lec-11/provider.tf
+++ Lec-11/provider.tf
@@ -1,6 +1,18 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.46.0"
+    }
+  }
 }
+
+provider "aws" {
+  # Configuration options
+}
+
+
+
 
 
 
