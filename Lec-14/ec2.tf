09f1c93c453cbb750617d827f3128aba00364fa4
Raghunath Kushawaha <78621381+iamraghu149@users.noreply.github.com>
2024-04-24 13:02:02 +0530

    Updated by Raghu ReadME
    Updated by Raghu ReadME

--- Lec-14/ec2.tf
+++ Lec-14/ec2.tf
@@ -1,8 +1,15 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket0149"
+}
+
+
 resource "aws_instance" "Web" {
   ami = "ami-007020fd9c84e18c7"
+  count = 3
   instance_type = var.size
+  depends_on = [aws_s3_bucket.mybucket]
 
   tags = {
-    Name = "Hello-Raghu"
+    Name = "Hello-Raghu-${count.index+1}"
   }
 }
